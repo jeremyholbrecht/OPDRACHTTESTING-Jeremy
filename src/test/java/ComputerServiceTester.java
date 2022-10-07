b10e314a1b0596ab5ace237eaa19fa692d3932d6
jeremyholbrecht <jerexyz@protonmail.com>
2022-10-07 15:31:00 +0200

    initial commit

--- src/test/java/ComputerServiceTester.java
+++ src/test/java/ComputerServiceTester.java
@@ -1,17 +1,74 @@
+import appPackage.data.Brand;
+import appPackage.data.Computer;
+import appPackage.repositories.ComputerRepository;
+import appPackage.services.implementations.ComputerServiceImpl;
+import appPackage.services.interfaces.ComputerService;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+@RunWith(MockitoJUnitRunner.class)
 public class ComputerServiceTester {
 
+    private static List<Computer> allComputers;
+    private static Computer dell;
+    private static Computer apple;
+    private static Computer samsung;
+    private static Computer toshiba;
 
+    @Mock
+    private ComputerRepository computerRepository;
+    @InjectMocks
+    private ComputerService computerService = new ComputerServiceImpl();
 
+    @BeforeClass
+    public static void createHypotheticalComputerLists(){
+        dell = new Computer("123", Brand.DELL, "dell info");
+        apple = new Computer("456", Brand.APPLE, "apple info");
+        samsung = new Computer("789", Brand.SAMSUNG, "samsung info");
+        toshiba = new Computer("127", Brand.TOSHIBA, "toshiba info");
+        dell.setNeedsReparation(true);
+        apple.setNeedsReparation(false);
+        samsung.setNeedsReparation(false);
+        toshiba.setNeedsReparation(true);
+
+    }
 
     @Test
     public void testIfYouGetBrokenComputers(){
+        List<Computer> allBrokenComputers = new ArrayList<>();
+        allBrokenComputers.add(dell);
+        allBrokenComputers.add(toshiba);
+        when(computerRepository.findByNeedsReparationTrue()).thenReturn(allBrokenComputers);
+        List<Computer> foundBrokenComputers = computerService.getAllBrokenComputers();
+        Assert.assertTrue(foundBrokenComputers.contains(dell));
+        Assert.assertTrue(foundBrokenComputers.contains(toshiba));
+        Assert.assertFalse(foundBrokenComputers.contains(apple));
+        Assert.assertFalse(foundBrokenComputers.contains(samsung));
+        Assert.assertEquals(2, foundBrokenComputers.size());
 
     }
 
     @Test
     public void testIfYouGetFixedComputers(){
+        List<Computer> allWorkingComputers = new ArrayList<>();
+        allWorkingComputers.add(apple);
+        allWorkingComputers.add(samsung);
+        when(computerRepository.findByNeedsReparationFalse()).thenReturn(allWorkingComputers);
+        List<Computer> foundWorkingComputers = computerService.getAllWorkingComputers();
+        Assert.assertEquals(2, foundWorkingComputers.size());
+        Assert.assertTrue(foundWorkingComputers.contains(apple));
+        Assert.assertTrue(foundWorkingComputers.contains(samsung));
+        Assert.assertFalse(foundWorkingComputers.contains(dell));
+        Assert.assertFalse(foundWorkingComputers.contains(toshiba));
 
     }
 }
--- src/test/java/LoanReceiptServiceTester.java
+++ src/test/java/LoanReceiptServiceTester.java
@@ -1,15 +1,72 @@
+import appPackage.data.Computer;
+import appPackage.data.LoanReceipt;
+import appPackage.data.Student;
+import appPackage.repositories.LoanReceiptRepository;
+import appPackage.services.implementations.LoanReceiptImpl;
+import appPackage.services.interfaces.LoanReceiptService;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class LoanReceiptServiceTester {
+
+    @Mock
+    private LoanReceiptRepository loanReceiptRepository;
+    @InjectMocks
+    private LoanReceiptService loanReceiptService = new LoanReceiptImpl();
+    private static Student student1;
+    private static Student student2;
+    private static Computer computerInUse;
+    private static Computer computerNotInUse;
+    private static LoanReceipt loanReceiptOfComputerInUse;
+    private static LoanReceipt loanReceiptOfComputerNotInUse;
+    private static List<LoanReceipt> student1LoanReceipts;
+    private static List<LoanReceipt> student2LoanReceipts;
+
+    @BeforeClass
+    public static void createData(){
+        computerInUse = new Computer();
+        computerNotInUse = new Computer();
+        loanReceiptOfComputerInUse = new LoanReceipt(LocalDate.of(2022, Month.JANUARY, 13),
+                student1, computerInUse);
+        loanReceiptOfComputerNotInUse = new LoanReceipt(LocalDate.of(2021, Month.SEPTEMBER, 3),
+                LocalDate.of(2022, Month.JUNE, 30), student2, computerNotInUse);
+        student1 = new Student();
+        List<LoanReceipt> loanReceiptOfStudent1 = new ArrayList<>();
+        loanReceiptOfStudent1.add(loanReceiptOfComputerInUse);
+        student1.setComputersTheyLoaned(loanReceiptOfStudent1);
+
+    }
 
 
     @Test
     public void testToSeeIfComputerIsInUse(){
+        when(loanReceiptRepository.findByComputerAndEndDateIsNull(computerInUse)).thenReturn(loanReceiptOfComputerInUse);
+        when(loanReceiptRepository.findByComputerAndEndDateIsNull(computerNotInUse)).thenReturn(null);
+        Assert.assertTrue(loanReceiptService.isPcInUse(computerInUse));
+        Assert.assertFalse(loanReceiptService.isPcInUse(computerNotInUse));
 
     }
 
+
     @Test
     public void testToSeeIfStudentOwnsComputer(){
+        when(loanReceiptRepository.findByStudentAndEndDateIsNull(student1)).thenReturn(loanReceiptOfComputerInUse);
+        Assert.assertEquals(computerInUse, loanReceiptService.checkIfStudentCurrentlyOwnsPC(student1));
+
 
     }
 
